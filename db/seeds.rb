5aac0d531cfcba99073bd09361edaa0fcf40336c
lakergirl702 <jesshoran87@gmail.com>
2020-09-28 11:59:48 -0700

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Workout.create([
+
+{"name": "Jump Lunge",
+"impact": "High",
+"fitness_type": "Power, Cardio",
+"focus": "Lower Body",
+"img": "https://media1.popsugar-assets.com/files/thumbor/XBjqBbublGCisq8WJ589r9Ljlws/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/12/28/137/n/1922729/tmp_1KPWA3_4a8efb52e5e289b7_PS17_0001_GetFit_Fitness_Workout_0738.jpg",
+"demo":"https://www.youtube.com/watch?v=6SFgE2_og_s",
+"reps": 20
+},
+
+{"name": "Burpee",
+"impact": "High",
+"fitness_type": "Power, Cardio",
+"focus": "Lower Body, Upper Body, Core",
+"img": "https://cdn.activebeat.com/eyJidWNrZXQiOiJwdWItc3RvcmFnZSIsImtleSI6ImFjdGl2ZWJlYXQvd3AtY29udGVudC91cGxvYWRzLzIwMTYvMDQvc2h1dHRlcnN0b2NrXzEzNTAyNDY3MzEtNzMweDQ5MC5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjM4MiwiaGVpZ2h0IjoyNTYsImZpdCI6ImNvdmVyIiwiYmFja2dyb3VuZCI6eyJyIjowLCJnIjowLCJiIjowLCJhbHBoYSI6MX0sInBvc2l0aW9uIjoidG9wIn19fQ==",
+"demo":"https://www.youtube.com/watch?v=6SFgE2_og_s",
+"reps": 10
+},
+
+{"name": "Squat Jack",
+"impact": "High",
+"fitness_type": "Power, Cardio",
+"focus": "Lower Body",
+"img": "https://media1.popsugar-assets.com/files/thumbor/XBjqBbublGCisq8WJ589r9Ljlws/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/12/28/137/n/1922729/tmp_1KPWA3_4a8efb52e5e289b7_PS17_0001_GetFit_Fitness_Workout_0738.jpg",
+"demo":"https://www.youtube.com/watch?v=6SFgE2_og_s",
+"reps": 20
+},
+
+{"name": "Lunge",
+"impact": "Low",
+"fitness_type": "Strength",
+"focus": "Lower Body",
+"img": "https://media1.popsugar-assets.com/files/thumbor/XBjqBbublGCisq8WJ589r9Ljlws/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/12/28/137/n/1922729/tmp_1KPWA3_4a8efb52e5e289b7_PS17_0001_GetFit_Fitness_Workout_0738.jpg",
+"demo":"https://www.youtube.com/watch?v=6SFgE2_og_s",
+"reps": 20
+},
+
+{"name": "Squat",
+"impact": "Low",
+"fitness_type": "Strength",
+"focus": "Lower Body",
+"img": "https://media1.popsugar-assets.com/files/thumbor/XBjqBbublGCisq8WJ589r9Ljlws/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/12/28/137/n/1922729/tmp_1KPWA3_4a8efb52e5e289b7_PS17_0001_GetFit_Fitness_Workout_0738.jpg",
+"demo":"https://www.youtube.com/watch?v=6SFgE2_og_s",
+"reps": 20
+}
+
+
+])
