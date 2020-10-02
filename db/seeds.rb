f06000c52f31d61539858756ec6ce962eb780607
lakergirl702 <jesshoran87@gmail.com>
2020-10-02 09:34:49 -0700

    adding more seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Workout.create([
 "fitness_type": "Strength",
 "focus": "Lower",
 "img": "https://i.imgur.com/6sruFVI.jpg",
-"demo":"https://www.youtube.com/watch?v=DNxmU1Sks7s",
+"demo": "https://www.youtube.com/watch?v=DNxmU1Sks7s",
 "reps": "20 total lunges - 10 on each side alternating each time",
 "muscles_worked": "Butt/Hips, Legs, Thighs" 
 },
@@ -23,7 +23,7 @@ Workout.create([
 "fitness_type": "Cardio, Strength, Power",
 "focus": "Lower",
 "img": "https://i.imgur.com/Ho9hZCG.jpg",
-"demo":"https://www.youtube.com/watch?v=-6O06OOW3AA",
+"demo": "https://www.youtube.com/watch?v=-6O06OOW3AA",
 "reps": "20 seconds - do as many as you can",
 "muscles_worked": "Butt/Hips, Legs, Thighs" 
 },
@@ -33,7 +33,7 @@ Workout.create([
 "fitness_type": "Strength",
 "focus": "Core",
 "img": "https://i.imgur.com/64Msfc1.jpg",
-"demo":"https://www.youtube.com/watch?v=BeS4ZcROuQw",
+"demo": "https://www.youtube.com/watch?v=BeS4ZcROuQw",
 "reps": "20 seconds - do as many as you can",
 "muscles_worked": "Core" 
 },
@@ -43,9 +43,119 @@ Workout.create([
 "fitness_type": "Strength",
 "focus": "Core",
 "img": "https://i.imgur.com/64Msfc1.jpg",
-"demo":"https://www.youtube.com/watch?v=BeS4ZcROuQw",
+"demo": "https://www.youtube.com/watch?v=BeS4ZcROuQw",
 "reps": "20 seconds - do as many as you can",
 "muscles_worked": "Core" 
+},
+
+{"name": "Alt Wide Push-Ups",
+"impact": "High",
+"fitness_type": "Strength, Power",
+"focus": "Upper",
+"img": "https://i.imgur.com/Wpg0F1F.jpg",
+"demo": "https://www.youtube.com/watch?v=Z71pfIboiBI",
+"reps": "20 seconds - do as many as you can",
+"muscles_worked": "Shoulders, Arms, Chest" 
+},
+
+{"name": "Alt Lateral Lunges",
+"impact": "Low",
+"fitness_type": "Strength",
+"focus": "Lower",
+"img": "https://i.imgur.com/x41UUD8.jpg",
+"demo": "https://www.youtube.com/watch?v=KquNZQaxj4E",
+"reps": "10 on left side, then 10 on right side",
+"muscles_worked": "Butt/Hips, Legs, Thighs" 
+},
+
+{"name": "Backstroke",
+"impact": "Low",
+"fitness_type": "Strength",
+"focus": "Core",
+"img": "https://i.imgur.com/fx60tJY.jpg",
+"demo": "https://www.youtube.com/watch?v=8wVWlSF8B_M",
+"reps": "20 seconds - do as many as you can",
+"muscles_worked": "Core" 
+},
+
+{"name": "Bicycle",
+"impact": "Low",
+"fitness_type": "Strength",
+"focus": "Core",
+"img": "https://i.imgur.com/X9n2O7U.jpg",
+"demo": "https://www.youtube.com/watch?v=FnPoz_n9PFg",
+"reps": "20 total",
+"muscles_worked": "Core" 
+},
+
+{"name": "Bird Dog",
+"impact": "Low",
+"fitness_type": "Strength",
+"focus": "Core",
+"img": "https://i.imgur.com/BnhRkae.jpg",
+"demo": "https://www.youtube.com/watch?v=DZ321xJdO68",
+"reps": "20 total",
+"muscles_worked": "Core, Back" 
+},
+
+{"name": "Burpee",
+"impact": "High",
+"fitness_type": "Cardio, Strength, Power",
+"focus": "Full",
+"img": "https://i.imgur.com/ABBbR8f.jpg",
+"demo": "https://www.youtube.com/watch?v=O-mXdFMxF48",
+"reps": "20 seconds - do as many as you can",
+"muscles_worked": "Full Body" 
+},
+
+{"name": "Butt Kicks",
+"impact": "High",
+"fitness_type": "Cardio",
+"focus": "Lower",
+"img": "https://i.imgur.com/7KYxdJG.jpg",
+"demo": "https://www.youtube.com/watch?v=AAHxX15vVyg",
+"reps": "20 seconds - do as many as you can",
+"muscles_worked": "Legs, Calves" 
+},
+
+{"name": "Climbers",
+"impact": "High",
+"fitness_type": "Cardio",
+"focus": "Full",
+"img": "https://i.imgur.com/zmytD53.jpg",
+"demo": "https://www.youtube.com/watch?v=wag6Jk9-bqA",
+"reps": "20 seconds - do as many as you can",
+"muscles_worked": "Full Body" 
+},
+
+{"name": "Diamond Push-Ups",
+"impact": "High",
+"fitness_type": "Strength, Power",
+"focus": "Upper",
+"img": "https://i.imgur.com/g6WrDiJ.jpg",
+"demo": "https://www.youtube.com/watch?v=ACZ6t2TbLRE",
+"reps": "20 seconds - do as many as you can",
+"muscles_worked": "Shoulders, Arms, Chest" 
+},
+
+{"name": "Forward Linear Jumps",
+"impact": "High",
+"fitness_type": "Cardio, Strength, Power",
+"focus": "Lower",
+"img": "https://i.imgur.com/49LJ9qx.jpg",
+"demo": "https://www.youtube.com/watch?v=oLwA8uBRxNg",
+"reps": "20 seconds - do as many as you can",
+"muscles_worked": "Butt/Hips, Legs, Thighs" 
+},
+
+{"name": "Frog Jumps",
+"impact": "High",
+"fitness_type": "Cardio, Strength, Power",
+"focus": "Lower",
+"img": "https://i.imgur.com/OQ4BsvZ.jpg",
+"demo": "https://www.youtube.com/watch?v=wMTOs21J8PI",
+"reps": "20 seconds - do as many as you can",
+"muscles_worked": "Butt/Hips, Legs, Thighs" 
 },
 
 ])
